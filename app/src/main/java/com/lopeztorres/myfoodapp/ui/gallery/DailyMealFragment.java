ffe8dfc65f29d351e9455808eb25037bfaf53992
Guzmance <00031119@uca.edu.sv>
2023-06-06 01:38:45 -0600

    Add: Navigation to Home Action

--- app/src/main/java/com/lopeztorres/myfoodapp/ui/gallery/DailyMealFragment.java
+++ app/src/main/java/com/lopeztorres/myfoodapp/ui/gallery/DailyMealFragment.java
@@ -1,23 +1,23 @@
-package com.lopeztorres.myfoodapp.ui.home;
+package com.lopeztorres.myfoodapp.ui.gallery;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
 
 import com.lopeztorres.myfoodapp.R;
-import com.lopeztorres.myfoodapp.databinding.FragmentHomeBinding;
 
-public class HomeFragment extends Fragment {
+public class DailyMealFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.fragment_home,container,false);
+
+        View root = inflater.inflate(R.layout.daily_meal_fragment,container,false);
+
+
         return root;
     }
 
