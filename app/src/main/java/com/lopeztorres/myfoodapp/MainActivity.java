12dda9fc9d2e4c4a7f3fe15c74b082d0521e173f
Guzmance <00031119@uca.edu.sv>
2023-06-08 20:29:34 -0600

    Add: Home Fix Bugs

--- app/src/main/java/com/lopeztorres/myfoodapp/MainActivity.java
+++ app/src/main/java/com/lopeztorres/myfoodapp/MainActivity.java
@@ -50,7 +50,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
     }
